8a2aae2b1ad2e433086733e3676031126243c845
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-07-16 00:20:18 +0300

    fixed another UI bug

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -682,6 +682,8 @@ fn draw_pomodoro_timer(f: &mut Frame, app: &App) {
             );
         f.render_widget(motivation_widget, motivation_area);
     }
+}
+
 fn draw_time_blocking_popup(f: &mut Frame, app: &App) {
     let area = centered_rect(60, 40, f.size());
     
